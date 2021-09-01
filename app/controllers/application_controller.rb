b05678e5101a5334f45e43acbfb41cb4de9079dd
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-09-01 14:30:55 +0200

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+# app/controllers/application_controller.rb
+class ApplicationController < ActionController::API
+  def render_jsonapi_response(resource)
+    if resource.errors.empty?
+      render jsonapi: resource
+    else
+      render jsonapi_errors: resource.errors, status: 400
+    end
+  end
+
+end
