87581982bd47cd9917b1ac8bc907d962ec1dd939
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-09-04 10:43:04 +0200

    before adding user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   default_url_options :host => "http://localhost:3000"
   namespace :api, defaults: { format: :json } do
     resources :users, only: %w[show]
